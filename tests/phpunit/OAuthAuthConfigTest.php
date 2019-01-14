072b505233bb3fb765197abcaaddb2d0011a0fa5
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2019-01-14 10:11:26 +0100

    Improve test stability with assertSame and …::class
    assertSame() compares the type too, which I would argue is a good thing
    in these cases.
    
    The …::class feature does make the code less fragile. Class names hidden
    in strings are always a source of errors.
    
    Change-Id: I94f8df7d3ed5e0382ab2135add1d9560903e4667

--- tests/phpunit/OAuthAuthConfigTest.php
+++ tests/phpunit/OAuthAuthConfigTest.php
@@ -2,6 +2,8 @@
 
 namespace MediaWiki\Extensions\OAuthAuthentication;
 
+use MediaWiki\OAuthClient\ClientConfig;
+
 /**
  * @covers \MediaWiki\Extensions\OAuthAuthentication\Config
  *
@@ -13,7 +15,7 @@ class OAuthAuthConfigTest extends OAuthAuthDBTest {
 	public function testGetDefaultConfig() {
 		$this->setMwGlobals( 'wgOAuthAuthenticationUrl', 'https://example.com/' );
 		$config = Config::getDefaultConfig();
-		$this->assertInstanceOf( 'MediaWiki\\OAuthClient\\ClientConfig', $config );
+		$this->assertInstanceOf( ClientConfig::class, $config );
 	}
 
 }
--- tests/phpunit/OAuthAuthExternalUserTest.php
+++ tests/phpunit/OAuthAuthExternalUserTest.php
@@ -17,8 +17,8 @@ class OAuthAuthExternalUserTest extends OAuthAuthDBTest {
 	 */
 	public function testExternalUser() {
 		$exUser = new OAuthExternalUser( 20, 30, 'ExUser' );
-		$this->assertEquals( 'ExUser', $exUser->getName() );
-		$this->assertEquals( 30, $exUser->getLocalId() );
+		$this->assertSame( 'ExUser', $exUser->getName() );
+		$this->assertSame( 30, $exUser->getLocalId() );
 	}
 
 	/**
@@ -29,8 +29,8 @@ class OAuthAuthExternalUserTest extends OAuthAuthDBTest {
 	public function testNewFromRemoteId() {
 		// We added remoteId 120 in parent class
 		$exUser = OAuthExternalUser::newFromRemoteId( 120, $this->userName, $this->db );
-		$this->assertInstanceOf( 'MediaWiki\Extensions\OAuthAuthentication\OAuthExternalUser', $exUser );
-		$this->assertEquals( $this->userName, $exUser->getName() );
+		$this->assertInstanceOf( OAuthExternalUser::class, $exUser );
+		$this->assertSame( $this->userName, $exUser->getName() );
 	}
 
 }
